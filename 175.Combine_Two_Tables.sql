27ab6dd6ec064a1d5bdb484bf7c51047ad279b8b
ermadhav <madhavtiwari2024@gmail.com>
2025-02-10 21:27:16 +0530

    Comment

--- 175.Combine_Two_Tables.sql
+++ 175.Combine_Two_Tables.sql
@@ -1,8 +1,10 @@
-# Write your MySQL query statement below
+-- Get first name, last name, city, and state  
+-- Show NULL if no address is found  
+
 SELECT 
-p.firstName,
-p.lastName,
-a.city,
-a.state
-FROM Person p
-LEFT JOIN Address a ON p.personId = a.personId;
+    p.firstName,  -- Person's first name  
+    p.lastName,   -- Person's last name  
+    a.city,       -- City from Address table  
+    a.state       -- State from Address table  
+FROM Person p  
+LEFT JOIN Address a ON p.personId = a.personId;  -- Keep all persons, even without an address  
